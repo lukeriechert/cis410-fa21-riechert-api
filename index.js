944e520fb17cefc9c80fc9dd9c610b17c555d723
unknown <luke.riechert@rams.colostate.edu>
2021-11-04 18:13:25 -0600

    Week 10.1 11/4 /movies/:pk

--- index.js
+++ index.js
@@ -18,10 +18,36 @@ app.get("/players", (req,res)=>{
     FROM Player
     LEFT JOIN Position
     ON position.PositionPK = player.PositionFK`)
-    .then((theResults)=>{res.status(200).send(theResults);})
+    .then((theResults)=>{
+        res.status(200).send(theResults);
+    })
     .catch((myError)=>{
         console.log(myError);
         res.status(500).send();
     });
-})
+});
+
+app.get("/players/:pk", (req, res)=>{
+    let pk = req.params.pk;
+    // console.log(pk);
+    let myQuery = `SELECT *
+    FROM Player
+    LEFT JOIN Position
+    ON position.PositionPK = player.PositionFK
+    WHERE playerpk = ${pk}`
+
+    db.executeQuery(myQuery)
+    .then((result)=>{
+        // console.log("result", result);
+        if(result[0]){
+            res.send(result[0]);
+        }else{
+            res.status(404).send(`Bad request`);
+        }
+    })
+    .catch((err)=>{
+        console.log("Error in /movies/:pk", err);
+        res.status(500).send();
+    })
+});
 
